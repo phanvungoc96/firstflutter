c3fc86bf60f695ffb8f91a22d6df2cd9427e8291
ChiHaoLuong <lulubao1234@gmail.com>
2022-07-22 11:13:24 +0700

    Theo dõi pager

---
+++ lib/screen/newsWidget/widget/news_card.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../../../models/news_model.dart';
+
+class NewsCard extends StatelessWidget {
+  final NewsModel news;
+
+  const NewsCard({Key? key, required this.news}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.transparent,
+          elevation: 0,
+        ),
+        onPressed: () {},
+        child: Column(
+          children: <Widget>[
+            Image.network(news.imgUrl),
+            const SizedBox(height: 8),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: SizedBox(
+                  width: width / 3,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        news.newsType,
+                        style: const TextStyle(color: Colors.red, fontSize: 14),
+                      ),
+                      Text(
+                        news.hours,
+                        style:
+                            const TextStyle(color: Colors.grey, fontSize: 14),
+                      )
+                    ],
+                  )),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              news.title,
+              style: const TextStyle(fontSize: 16, color: Colors.black),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
