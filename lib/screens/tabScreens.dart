5ac72f9baeab59e8151530efddd8fcda69e43bb3
Ngoc Phan <ngocpvx@nichietsuvn.com>
2022-07-25 10:49:42 +0700

    update UI

--- lib/screens/tabScreens.dart
+++ lib/screens/tabScreens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/screens/videos/videoTab.dart';
 
 import './newsWidget/news_widget.dart';
 import './detail/detail.dart';
@@ -18,7 +19,7 @@ class _TabsScreenState extends State<TabsScreen> {
       'title': 'News',
     },
     {
-      'page': DetailPage(),
+      'page': Videos(),
       'title': 'Video',
     },
     {
@@ -53,7 +54,7 @@ class _TabsScreenState extends State<TabsScreen> {
               Color.fromRGBO(0, 106, 154, 1),
               Color.fromRGBO(0, 93, 152, 1),
             ])),
-            height: 68,
+            height: 72,
             child: SingleChildScrollView(
               physics: NeverScrollableScrollPhysics(),
               child: BottomNavigationBar(
@@ -63,7 +64,7 @@ class _TabsScreenState extends State<TabsScreen> {
                 selectedItemColor: Colors.yellow,
                 currentIndex: _selectedPageIndex,
                 showUnselectedLabels: true,
-                iconSize: 20,
+                iconSize: 24,
                 items: const [
                   BottomNavigationBarItem(
                     backgroundColor: Colors.transparent,
@@ -72,7 +73,7 @@ class _TabsScreenState extends State<TabsScreen> {
                   ),
                   BottomNavigationBarItem(
                     backgroundColor: Colors.transparent,
-                    icon: Icon(Icons.smart_display),
+                    icon: Icon(Icons.play_arrow),
                     label: 'Video',
                   ),
                   BottomNavigationBarItem(
